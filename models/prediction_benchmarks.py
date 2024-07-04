b6244a9298f7df0466c39079d675dce468dc52ad
pachacutexx <152634036+pachacutexx@users.noreply.github.com>
2024-07-04 15:37:42 +0200

    Update prediction_benchmarks.py
    Added second benchmark model

--- models/prediction_benchmarks.py
+++ models/prediction_benchmarks.py
@@ -58,3 +58,62 @@ class TimeGrowth:
         npi_list = self.sorted_df['npi'].unique()
         predictions_df = pd.DataFrame({'npi': npi_list, 'y_pred_tg': y_pred})
         return predictions_df
+
+
+class ARModel:
+    def __init__(self, df):
+        self.df = df
+        self.sorted_df = self._prepare_dataframe()
+
+    def _prepare_dataframe(self):
+        # Select the specified columns
+        selected_columns = self.df[['npi', 'service_year', 'service_quarter', 'total_claims']]
+        # Sort the new DataFrame by 'npi', 'service_year', and 'service_quarter' in ascending order
+        sorted_df = selected_columns.sort_values(by=['npi', 'service_year', 'service_quarter'], ascending=True)
+        return sorted_df
+
+    def _create_lagged_features(self, df, lag=1):
+        for i in range(1, lag + 1):
+            df[f'lag_{i}'] = df['total_claims'].shift(i)
+        return df
+
+    def fit_predict(self):
+        y_pred_list = []
+        y_test_list = []
+
+        # Group the DataFrame by 'npi'
+        grouped = self.sorted_df.groupby('npi')
+
+        # Iterate over each group
+        for npi, group in grouped:
+            # Create lagged features
+            group = self._create_lagged_features(group, lag=2)
+            
+            # Drop rows with NaN values created by shifting
+            group = group.dropna()
+            
+            # Use all but the last observation as training set
+            train = group.iloc[:-1]
+            # Use the last observation as the test set
+            test = group.iloc[-1]
+            
+            # Prepare training and testing data
+            X_train = train[['lag_1', 'lag_2']]
+            y_train = train['total_claims']
+            X_test = test[['lag_1', 'lag_2']].to_frame().T
+            y_test = test['total_claims']
+            
+            # Fit the linear regression model
+            model = LinearRegression()
+            model.fit(X_train, y_train)
+            
+            # Predict the value for the test set
+            y_pred = model.predict(X_test)
+            y_pred = np.round(y_pred, 0)
+            y_pred = np.maximum(y_pred, 0)
+            
+            # Store the results
+            y_pred_list.append(y_pred[0])
+            y_test_list.append(y_test)
+
+        return y_pred_list, y_test_list
